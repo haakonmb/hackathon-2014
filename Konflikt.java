4eb5915656e52b102ceeaf0ab2f54b16271ce005
mordbad <haakonborch@gmail.com>
2014-03-31 16:10:29 +0200

    new file:   .gitignore 	modified:   Konflikt.java

--- Konflikt.java
+++ Konflikt.java
@@ -1,16 +1,25 @@
 public class Konflikt{
+
+	int[] indexes;
 	public void Konflikt(){
 
 }
-
-	public void sjekk(String[][] str1,String[][]str2){
+	//Skal returnere true når en konflikt eksisterer
+	public boolean sjekk(String[][] str1,String[][]str2){
 		int i =0;
 		for(String[] arr : str1){
 			i++;	
 			for(int j = 0; j < 2; j++){
 				System.out.println(arr[j]+ " "+ i+" "+  j);	
-
+				
 			}
-		}	
+		}
+		//TODO:fiks return
+		return true;	
 	}
+
+	private void konf(int i, int j){
+		
+	}
+
 }
